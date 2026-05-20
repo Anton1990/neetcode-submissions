13c2b12d00b3c2f7eb3ea3c64d4ccd8e3597b226
Anton Lukyantsev <Lukyantsev1990@yandex.ru>
2026-05-20 23:39:21 +0400

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int[] ProductExceptSelf(int[] nums) {
+        int[] prefix = new int[nums.Length];
+        int[] postfix = new int[nums.Length];
+        int[] res = new int[nums.Length];
+        prefix[0]=1;
+        postfix[nums.Length-1]=1;
+
+        for(int i = 1; i<nums.Length;i++){
+            prefix[i]=nums[i-1]*prefix[i-1];
+        }
+
+        for(int i = nums.Length-2; i>=0;i--){
+            postfix[i]=nums[i+1]*postfix[i+1];
+        }
+
+        for(int i = 0; i<nums.Length;i++){
+            res[i]=prefix[i]* postfix[i];
+        }
+
+        return res;
+
+    }
+}
